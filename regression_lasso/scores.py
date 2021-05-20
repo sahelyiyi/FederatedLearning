ec59290341cf93120ca6200b3a68d405a938397a
Sahelyiyi <sahel@Sahelyiyis-MacBook-Pro.local>
2021-05-20 13:39:54 +0300

    update SBM notebook

--- regression_lasso/scores.py
+++ regression_lasso/scores.py
@@ -4,71 +4,118 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error
 
 
-def get_algorithm_1_scores(X, Y, new_w, samplingset, not_samplingset):
-    Y_pred = []
-    for i in range(len(X)):
-        Y_pred.append(np.dot(X[i], new_w[i]))
-    Y_pred = np.array(Y_pred)
+def get_algorithm1_MSE(datapoints, predicted_w, samplingset):
+    '''
+    :param datapoints:  a dictionary containing the data of each node in the graph needed for the algorithm 1
+    :param predicted_w: the predicted weigh vectors for each node
+    :param samplingset: the sampling set for the algorithm 1
 
-    alg_1_score = {'total': mean_squared_error(Y, Y_pred),
-                   'train': mean_squared_error(Y[samplingset], Y_pred[samplingset]),
-                   'test': mean_squared_error(Y[not_samplingset], Y_pred[not_samplingset])}
+    :return alg1_MSE: the MSE of the algorithm 1 for all the nodes, the samplingset and other nodes (test set)
+    '''
+    not_samplingset = [i for i in range(len(datapoints)) if i not in samplingset]
 
-    return alg_1_score
+    true_labels = []
+    pred_labels = []
+    for i in range(len(datapoints)):
+        features = np.array(datapoints[i]['features'])
+        label = np.array(datapoints[i]['label'])
+        true_labels.append(label)
+
+        pred_labels.append(np.dot(features, predicted_w[i]))
+
+    pred_labels = np.array(pred_labels)
+    true_labels = np.array(true_labels)
+
+    alg1_MSE = {'total': mean_squared_error(true_labels, pred_labels),
+                'train': mean_squared_error(true_labels[samplingset], pred_labels[samplingset]),
+                'test': mean_squared_error(true_labels[not_samplingset], pred_labels[not_samplingset])}
+
+    return alg1_MSE
 
 
-def get_linear_regression_score(x, y, decision_tree_samplingset, decision_tree_not_samplingset):
-    model = LinearRegression().fit(x[decision_tree_samplingset], y[decision_tree_samplingset])
+def get_linear_regression_MSE(x, y, samplingset, not_samplingset):
+    '''
+    :param x: a list containing the features of the nodes
+    :param y: a list containing the labels of the nodes
+    :param samplingset: the training dataset
+    :param not_samplingset: the test dataset
+    :return linear_regression_MSE : the MSE of linear regression for all the nodes, the samplingset and other nodes (test set)
+    '''
+
+    model = LinearRegression().fit(x[samplingset], y[samplingset])
     pred_y = model.predict(x)
-    linear_regression_score = {'total': mean_squared_error(y, pred_y),
-                               'train': mean_squared_error(y[decision_tree_samplingset],
-                                                           pred_y[decision_tree_samplingset]),
-                               'test': mean_squared_error(y[decision_tree_not_samplingset],
-                                                          pred_y[decision_tree_not_samplingset])}
 
-    return linear_regression_score
+    linear_regression_MSE = {'total': mean_squared_error(y, pred_y),
+                             'train': mean_squared_error(y[samplingset],
+                                                         pred_y[samplingset]),
+                             'test': mean_squared_error(y[not_samplingset],
+                                                        pred_y[not_samplingset])}
+
+    return linear_regression_MSE
 
 
-def get_decision_tree_score(x, y, decision_tree_samplingset, decision_tree_not_samplingset):
+def get_decision_tree_MSE(x, y, samplingset, not_samplingset):
+    '''
+    :param x: a list containing the features of the nodes
+    :param y: a list containing the labels of the nodes
+    :param samplingset: the training dataset
+    :param not_samplingset: the test dataset
+    :return decision_tree_MSE : the MSE of decision tree for all the nodes, the samplingset and other nodes (test set)
+    '''
+
     max_depth = 2
+
     regressor = DecisionTreeRegressor(max_depth=max_depth)
-    regressor.fit(x[decision_tree_samplingset], y[decision_tree_samplingset])
+    regressor.fit(x[samplingset], y[samplingset])
     pred_y = regressor.predict(x)
-    decision_tree_score = {'total': mean_squared_error(y, pred_y),
-                           'train': mean_squared_error(y[decision_tree_samplingset],
-                                                       pred_y[decision_tree_samplingset]),
-                           'test': mean_squared_error(y[decision_tree_not_samplingset],
-                                                      pred_y[decision_tree_not_samplingset])}
-    return decision_tree_score
+
+    decision_tree_MSE = {'total': mean_squared_error(y, pred_y),
+                         'train': mean_squared_error(y[samplingset],
+                                                     pred_y[samplingset]),
+                         'test': mean_squared_error(y[not_samplingset],
+                                                    pred_y[not_samplingset])}
+    return decision_tree_MSE
 
 
-def get_scores(datapoints, new_w, samplingset):
+def get_scores(datapoints, predicted_w, samplingset):
+    N = len(datapoints)
+    '''
+    N : the total number of nodes
+    '''
+
+    # calculate algorithm1 MSE
+    alg_1_score = get_algorithm1_MSE(datapoints, predicted_w, samplingset)
+
+    # prepare the data for calculating the linear regression and decision tree regression MSEs
     X = []
-    Y = []
+    '''
+    X: an array containing the features of all the nodes
+    '''
+    true_labels = []
+    '''
+    true_labels: an array containing the labels of all the nodes
+    '''
     for i in range(len(datapoints)):
         X.append(np.array(datapoints[i]['features']))
-        Y.append(np.array(datapoints[i]['label']))
+        true_labels.append(np.array(datapoints[i]['label']))
 
     X = np.array(X)
-    Y = np.array(Y)
-
-    N = len(X)
+    true_labels = np.array(true_labels)
     m, n = X[0].shape
 
-    not_samplingset = [i for i in range(N) if i not in samplingset]
-
-    alg_1_score = get_algorithm_1_scores(X, Y, new_w, samplingset, not_samplingset)
-
-    y = Y.reshape(-1, 1)
     x = X.reshape(-1, n)
-    decision_tree_samplingset = []
+    y = true_labels.reshape(-1, 1)
+
+    reformated_samplingset = []
     for item in samplingset:
         for i in range(m):
-            decision_tree_samplingset.append(m * item + i)
-    decision_tree_not_samplingset = [i for i in range(m * N) if i not in decision_tree_samplingset]
+            reformated_samplingset.append(m * item + i)
+    reformated_not_samplingset = [i for i in range(m * N) if i not in reformated_samplingset]
 
-    linear_regression_score = get_linear_regression_score(x, y, decision_tree_samplingset, decision_tree_not_samplingset)
+    # calculate linear regression MSE
+    linear_regression_score = get_linear_regression_MSE(x, y, reformated_samplingset, reformated_not_samplingset)
 
-    decision_tree_score = get_decision_tree_score(x, y, decision_tree_samplingset, decision_tree_not_samplingset)
+    # calculate decision tree MSE
+    decision_tree_score = get_decision_tree_MSE(x, y, reformated_samplingset, reformated_not_samplingset)
 
     return alg_1_score, linear_regression_score, decision_tree_score
